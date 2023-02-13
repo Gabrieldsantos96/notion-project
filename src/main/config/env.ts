c2dd62be063389f3b15e13abdff14012cd586a16
gsantos <gabrieltemp.mobile@gmail.com>
2023-02-13 13:54:49 -0300

    chore: mongo config

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: `mongodb+srv://gsantos:${process.env.MONGO_PASS}@cluster0.xvucjq8.mongodb.net/?retryWrites=true&w=majority`,
+  mongoUrl: `mongodb+srv://gsantos:marapicu@cluster0.xvucjq8.mongodb.net/?retryWrites=true&w=majority`,
   port: process.env.PORT || 3333
 }
