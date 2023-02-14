a6cef3cc57a89e523a33178f9d23d32506038a09
Gabriel Santos <94016081+Gabrieldsantos96@users.noreply.github.com>
2023-02-14 08:25:07 -0300

    Update env.ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl:
-    'mongodb+srv://gsantos:marapicu@cluster0.xvucjq8.mongodb.net/node-api?retryWrites=true&w=majority',
+  `mongodb+srv://gsantos:${process.env.MONGO_PASS}@cluster0.xvucjq8.mongodb.net/node-api?retryWrites=true&w=majority`,
   port: process.env.PORT || 3333
 }
